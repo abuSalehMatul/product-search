74737514ea14ffdc08f4a212dc1eb3a1afd88fe8
abusalehmatul <saleh.matul@gmail.com>
2021-06-29 16:59:30 +0600

    new

--- public/search.js
+++ public/search.js
@@ -4,64 +4,70 @@
   let allVendors = [], processedVal = [], totalSearchRes = [], finalResult;
 
   inp.addEventListener("keyup", function (e) {
-    //console.log('take one');
-    let container, currentRes = [], inputValue = e.target.value, splittedInpValue;
+    let  inputValue = e.target.value, splittedInpValue;
     if (e.keyCode == 32 || !inputValue || e.keyCode == 188) return false;
+
     splittedInpValue = inputValue.split(",");
     splittedInpValue = splittedInpValue.map(split => split.trim())
     for (let ittr = 0; ittr < splittedInpValue.length; ittr++) {
       let val = splittedInpValue[ittr];
       if (!val) return false;
-      if (processedVal.includes(val)) continue; //will contain only 2+ value... one before comma other after comma
-      else processedVal.push(val);
 
-      /*append the DIV element as a child of the autocomplete container:*/
-      // container = document.createElement("DIV");
-      // container.id = e.target.id + "autocomplete-list";
-      // container.setAttribute("class", "autocomplete-items");
-      // e.target.parentNode.appendChild(container);
-      // console.log(inp.parentNode);
-      // console.log(e.target.parentNode)
-      //.................................................................
-      $.ajax({
-        url: "https://groceryfinder.myshopify.com/search",
-        data: {
-          q: val,
-          type: "product",
-          view: "json",
-        },
-        dataType: "json",
-        success: function (data) {
-          let vendors = [];
-          //console.log(data);
-          vendors = data.map(el => el.vendor);
-          vendors = vendors.filter((v, i, a) => a.indexOf(v) === i);
-          if (typeof allVendors[ittr] == "undefined") {
-            allVendors.push(vendors);
-          } else {
-            allVendors[ittr] = vendors;
+      if (typeof totalSearchRes[val] != "undefined") {
+        let ind = splittedInpValue.indexOf(val);
+        let modifiedVal = [];
+        for(let k=0; k < splittedInpValue.length; k++){
+          if(k <= ind){
+              modifiedVal.push(splittedInpValue[k])
           }
-          // console.log('all vendores ', allVendors)
-          currentRes = [];
-          for (let i = 0; i < vendors.length; i++) {
-            let tem = {};
-            tem[vendors[i]] = [];
-            currentRes.push(tem);
-          }
-          for (let i = 0; i < data.length; i++) {
-            currentRes = buildCurrentRes(data[i].vendor, data[i], currentRes);
-          }
-          totalSearchRes[val] = currentRes;
-          //console.log(val, totalSearchRes);
-          // console.log('current res ', currentRes);
-          finalResult = getFinalResult(totalSearchRes, splittedInpValue);
-          removeDuplicate(finalResult);
-          drawAutoComplete(finalResult, ittr);
         }
-      });
+        finalResult = getFinalResult(totalSearchRes, modifiedVal);
+        drawAutoComplete(finalResult, ittr);
+      } else {
+        console.log('internet');
+        $.ajax({
+          url: "https://groceryfinder.myshopify.com/search",
+          data: {
+            q: val,
+            type: "product",
+            view: "json",
+          },
+          dataType: "json",
+          success: function (data) {
+            processor(data, ittr, val, splittedInpValue);
+          }
+        });
+      }
+
     }
 
   });
+
+  function processor(data, ittr, val, splittedInpValue) {
+    let currentRes = []
+    let vendors = [];
+    //console.log(data);
+    vendors = data.map(el => el.vendor);
+    vendors = vendors.filter((v, i, a) => a.indexOf(v) === i);
+    if (typeof allVendors[ittr] == "undefined") {
+      allVendors.push(vendors);
+    } else {
+      allVendors[ittr] = vendors;
+    }
+    for (let i = 0; i < vendors.length; i++) {
+      let tem = {};
+      tem[vendors[i]] = [];
+      currentRes.push(tem);
+    }
+    for (let i = 0; i < data.length; i++) {
+      currentRes = buildCurrentRes(data[i].vendor, data[i], currentRes);
+    }
+    totalSearchRes[val] = currentRes;
+    // console.log('toatla res ',  totalSearchRes[val]);
+    finalResult = getFinalResult(totalSearchRes, splittedInpValue);
+    removeDuplicate(finalResult);
+    drawAutoComplete(finalResult, ittr);
+  }
 
   function buildCurrentRes(vendor, productData, resArr) {
     for (let i = 0; i < resArr.length; i++) {
@@ -80,7 +86,7 @@
         let sObj = Object.entries(allRes[inputNo][i]);
         if (typeof mergedContainer[`${sObj[0][0]}`] == "undefined") {
           mergedContainer[`${sObj[0][0]}`] = sObj[0][1]
-          // console.log(mergedContainer)
+        
         } else {
           mergedContainer[`${sObj[0][0]}`] = mergedContainer[`${sObj[0][0]}`].concat(sObj[0][1]);
         }
@@ -132,34 +138,34 @@
     let vendorsAndProducts = Object.entries(results);
     console.log(vendorsAndProducts)
     for (let i = 0; i < vendorsAndProducts.length; i++) {
-      let arrow =  '<svg height="15px" version="1.1" id="mat-plus" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path d="M256,0C114.833,0,0,114.833,0,256s114.833,256,256,256s256-114.853,256-256S397.167,0,256,0z M256,472.341 c-119.275,0-216.341-97.046-216.341-216.341S136.725,39.659,256,39.659S472.341,136.705,472.341,256S375.295,472.341,256,472.341z"/></g></g><g><g><path d="M355.148,234.386H275.83v-79.318c0-10.946-8.864-19.83-19.83-19.83s-19.83,8.884-19.83,19.83v79.318h-79.318 c-10.966,0-19.83,8.884-19.83,19.83s8.864,19.83,19.83,19.83h79.318v79.318c0,10.946,8.864,19.83,19.83,19.83 s19.83-8.884,19.83-19.83v-79.318h79.318c10.966,0,19.83-8.884,19.83-19.83S366.114,234.386,355.148,234.386z"/></g></g></svg>';
+      let arrow = '<svg height="15px" version="1.1" id="mat-plus" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path d="M256,0C114.833,0,0,114.833,0,256s114.833,256,256,256s256-114.853,256-256S397.167,0,256,0z M256,472.341 c-119.275,0-216.341-97.046-216.341-216.341S136.725,39.659,256,39.659S472.341,136.705,472.341,256S375.295,472.341,256,472.341z"/></g></g><g><g><path d="M355.148,234.386H275.83v-79.318c0-10.946-8.864-19.83-19.83-19.83s-19.83,8.884-19.83,19.83v79.318h-79.318 c-10.966,0-19.83,8.884-19.83,19.83s8.864,19.83,19.83,19.83h79.318v79.318c0,10.946,8.864,19.83,19.83,19.83 s19.83-8.884,19.83-19.83v-79.318h79.318c10.966,0,19.83-8.884,19.83-19.83S366.114,234.386,355.148,234.386z"/></g></g></svg>';
       shopEl = document.createElement("DIV");
-      shopEl.id = "mat-shop-"+i;
+      shopEl.id = "mat-shop-" + i;
       shopEl.innerHTML = "<span class='mat-heighlight'>" + arrow + vendorsAndProducts[i][0] + "</span>";
       /*execute a function when someone clicks on the item value (DIV element):*/
       shopEl.addEventListener("click", function (e) {
-         e.stopPropagation();
-         if(document.getElementById("mat-product-container-"+i).style.display == "none"){
-          $("#mat-product-container-"+i).show(500);
-         }else{
-          $("#mat-product-container-"+i).hide(500);
-         }
+        e.stopPropagation();
+        if (document.getElementById("mat-product-container-" + i).style.display == "none") {
+          $("#mat-product-container-" + i).show(300);
+        } else {
+          $("#mat-product-container-" + i).hide(500);
+        }
       });
       container.appendChild(shopEl);
 
       let productContainer = document.createElement("div");
-      productContainer.id="mat-product-container-"+i;
+      productContainer.id = "mat-product-container-" + i;
       productContainer.style.display = "none";
-      for(let j =0 ; j < vendorsAndProducts[i][1].length ; j++){
-         let productDiv = document.createElement('div');
-         productDiv.classList.add("mat-product-insearch");
-         let image = document.createElement('img');
-         image.src =  vendorsAndProducts[i][1][j].featured_image;
-         productDiv.append(image);
-         let title = document.createElement('span');
-         title.innerHTML = vendorsAndProducts[i][1][j].title;
-         productDiv.append(title);
-         productContainer.append(productDiv);
+      for (let j = 0; j < vendorsAndProducts[i][1].length; j++) {
+        let productDiv = document.createElement('div');
+        productDiv.classList.add("mat-product-insearch");
+        let image = document.createElement('img');
+        image.src = vendorsAndProducts[i][1][j].featured_image;
+        productDiv.append(image);
+        let title = document.createElement('span');
+        title.innerHTML = vendorsAndProducts[i][1][j].title;
+        productDiv.append(title);
+        productContainer.append(productDiv);
       }
       shopEl.append(productContainer);
 
