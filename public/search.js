d4dc7c7c53a9a89a43681735c0b2dbc8d28e3c1b
abusalehmatul <saleh.matul@gmail.com>
2021-07-04 17:07:22 +0600

    first commit

--- public/search.js
+++ public/search.js
@@ -10,126 +10,126 @@
     if(inputValue == matTakenInput) return false;
     matTakenInput = inputValue;
     
-    splittedInpValue = inputValue.split(",");
-    splittedInpValue = splittedInpValue.map(split => split.trim())
-    matAllSplitedValue = splittedInpValue;
-    for (let ittr = 0; ittr < splittedInpValue.length; ittr++) {
-      let val = splittedInpValue[ittr];
-      if (!val) return false;
+    // splittedInpValue = inputValue.split(",");
+    // splittedInpValue = splittedInpValue.map(split => split.trim())
+    // matAllSplitedValue = splittedInpValue;
+    // for (let ittr = 0; ittr < splittedInpValue.length; ittr++) {
+    //   let val = splittedInpValue[ittr];
+    //   if (!val) return false;
 
-      if (typeof totalSearchRes[val] != "undefined") {
-        let ind = splittedInpValue.indexOf(val);
-        let modifiedVal = [];
-        for(let k=0; k < splittedInpValue.length; k++){
-          if(k <= ind){
-              modifiedVal.push(splittedInpValue[k])
-          }
-        }
-        finalResult = getFinalResult(totalSearchRes, modifiedVal);
-        drawAutoComplete(finalResult, ittr);
-      } else {
-        $.ajax({
-          url: "https://groceryfinder.myshopify.com/search",
-          data: {
-            q: val,
-            type: "product",
-            view: "json",
-          },
-          dataType: "json",
-          success: function (data) {
-            processor(data, ittr, val, splittedInpValue);
-          }
-        });
-      }
+    //   if (typeof totalSearchRes[val] != "undefined") {
+    //     let ind = splittedInpValue.indexOf(val);
+    //     let modifiedVal = [];
+    //     for(let k=0; k < splittedInpValue.length; k++){
+    //       if(k <= ind){
+    //           modifiedVal.push(splittedInpValue[k])
+    //       }
+    //     }
+    //     finalResult = getFinalResult(totalSearchRes, modifiedVal);
+    //     drawAutoComplete(finalResult, ittr);
+    //   } else {
+    //     $.ajax({
+    //       url: "https://groceryfinder.myshopify.com/search",
+    //       data: {
+    //         q: val,
+    //         type: "product",
+    //         view: "json",
+    //       },
+    //       dataType: "json",
+    //       success: function (data) {
+    //         processor(data, ittr, val, splittedInpValue);
+    //       }
+    //     });
+    //   }
 
-    }
+    // }
 
   });
 
-  function processor(data, ittr, val, splittedInpValue) {
-    let currentRes = []
-    let vendors = [];
-    //console.log(data);
-    vendors = data.map(el => el.vendor);
-    vendors = vendors.filter((v, i, a) => a.indexOf(v) === i);
-    if (typeof allVendors[ittr] == "undefined") {
-      allVendors.push(vendors);
-    } else {
-      allVendors[ittr] = vendors;
-    }
-    for (let i = 0; i < vendors.length; i++) {
-      let tem = {};
-      tem[vendors[i]] = [];
-      currentRes.push(tem);
-    }
-    for (let i = 0; i < data.length; i++) {
-      currentRes = buildCurrentRes(data[i].vendor, data[i], currentRes);
-    }
-    totalSearchRes[val] = currentRes;
-    // console.log('toatla res ',  totalSearchRes[val]);
-    finalResult = getFinalResult(totalSearchRes, splittedInpValue);
-    removeDuplicate(finalResult);
-    drawAutoComplete(finalResult, ittr);
-  }
+  // function processor(data, ittr, val, splittedInpValue) {
+  //   let currentRes = []
+  //   let vendors = [];
+  //   //console.log(data);
+  //   vendors = data.map(el => el.vendor);
+  //   vendors = vendors.filter((v, i, a) => a.indexOf(v) === i);
+  //   if (typeof allVendors[ittr] == "undefined") {
+  //     allVendors.push(vendors);
+  //   } else {
+  //     allVendors[ittr] = vendors;
+  //   }
+  //   for (let i = 0; i < vendors.length; i++) {
+  //     let tem = {};
+  //     tem[vendors[i]] = [];
+  //     currentRes.push(tem);
+  //   }
+  //   for (let i = 0; i < data.length; i++) {
+  //     currentRes = buildCurrentRes(data[i].vendor, data[i], currentRes);
+  //   }
+  //   totalSearchRes[val] = currentRes;
+  //   // console.log('toatla res ',  totalSearchRes[val]);
+  //   finalResult = getFinalResult(totalSearchRes, splittedInpValue);
+  //   removeDuplicate(finalResult);
+  //   drawAutoComplete(finalResult, ittr);
+  // }
 
-  function buildCurrentRes(vendor, productData, resArr) {
-    for (let i = 0; i < resArr.length; i++) {
-      if (Object.keys(resArr[i])[0] == vendor) {
-        resArr[i][`${vendor}`].push(productData)
-      }
-    }
-    return resArr;
-  }
+  // function buildCurrentRes(vendor, productData, resArr) {
+  //   for (let i = 0; i < resArr.length; i++) {
+  //     if (Object.keys(resArr[i])[0] == vendor) {
+  //       resArr[i][`${vendor}`].push(productData)
+  //     }
+  //   }
+  //   return resArr;
+  // }
 
-  function getFinalResult(allRes, splitedVal) {
-    let mergedContainer = {};
-    splitedVal.forEach(inputNo => {
-      // console.log('matched res ',allRes[inputNo])
-      for (let i = 0; i < allRes[inputNo].length; i++) {
-        let sObj = Object.entries(allRes[inputNo][i]);
-        if (typeof mergedContainer[`${sObj[0][0]}`] == "undefined") {
-          mergedContainer[`${sObj[0][0]}`] = sObj[0][1]
+  // function getFinalResult(allRes, splitedVal) {
+  //   let mergedContainer = {};
+  //   splitedVal.forEach(inputNo => {
+  //     // console.log('matched res ',allRes[inputNo])
+  //     for (let i = 0; i < allRes[inputNo].length; i++) {
+  //       let sObj = Object.entries(allRes[inputNo][i]);
+  //       if (typeof mergedContainer[`${sObj[0][0]}`] == "undefined") {
+  //         mergedContainer[`${sObj[0][0]}`] = sObj[0][1]
         
-        } else {
-          mergedContainer[`${sObj[0][0]}`] = mergedContainer[`${sObj[0][0]}`].concat(sObj[0][1]);
-        }
-      }
-    });
-    return mergedContainer;
-  }
+  //       } else {
+  //         mergedContainer[`${sObj[0][0]}`] = mergedContainer[`${sObj[0][0]}`].concat(sObj[0][1]);
+  //       }
+  //     }
+  //   });
+  //   return mergedContainer;
+  // }
 
-  function removeDuplicate(results) {
-    if (typeof results == "object") {
-      let resultSeperated = Object.entries(results);
-      resultSeperated.forEach(res => {
-        let key = res[0];
-        let values = res[1];
-        let ids = values.map(obj => obj.id);
-        let duplicates = [];
-        ids.sort(function (a, b) { return a - b });
-        for (let i = 0; i < ids.length - 1; i++) {
-          if (ids[i] == ids[i + 1]) {
-            duplicates.push(ids[i + 1])
-          }
-        }
-        for (let i = values.length - 1; i >= 0; i--) {
-          if (duplicates.includes(values[i].id)) {
-            let temId = values[i].id;
-            values.splice(i, 1);
-            for (let j = 0; j < duplicates.length; j++) {
-              if (duplicates[j] == temId) {
-                duplicates.splice(j, 1);
-              }
-            }
-          }
-        }
+  // function removeDuplicate(results) {
+  //   if (typeof results == "object") {
+  //     let resultSeperated = Object.entries(results);
+  //     resultSeperated.forEach(res => {
+  //       let key = res[0];
+  //       let values = res[1];
+  //       let ids = values.map(obj => obj.id);
+  //       let duplicates = [];
+  //       ids.sort(function (a, b) { return a - b });
+  //       for (let i = 0; i < ids.length - 1; i++) {
+  //         if (ids[i] == ids[i + 1]) {
+  //           duplicates.push(ids[i + 1])
+  //         }
+  //       }
+  //       for (let i = values.length - 1; i >= 0; i--) {
+  //         if (duplicates.includes(values[i].id)) {
+  //           let temId = values[i].id;
+  //           values.splice(i, 1);
+  //           for (let j = 0; j < duplicates.length; j++) {
+  //             if (duplicates[j] == temId) {
+  //               duplicates.splice(j, 1);
+  //             }
+  //           }
+  //         }
+  //       }
 
-        results[`${key}`] = values
+  //       results[`${key}`] = values
 
-      })
-    }
-    return results;
-  }
+  //     })
+  //   }
+  //   return results;
+  // }
 
   function drawAutoComplete(results, currentSearchIndex) {
     closeAllLists();
--- public/searchpage.js
+++ public/searchpage.js
@@ -1,9 +1,128 @@
 {
-    let shopWithProducts = matulGetParameterByName('search-res');
-    console.log(shopWithProducts);
+    let allVendors = [], totalSearchRes = [], finalResult;
+    let searchString = matulGetParameterByName('search-result'), splittedInpValue;
+    console.log(searchString);
     //get query string value
-    
-    var mSearchTemplate = '<div id="CollectionSection" data-section-id="collection-template" data-section-type="collection-template"> <div class="grid grid-border"> <div class="grid-item"> <header class="section-header"> <h1 class="section-header--title section-header--left h1">Fruits</h1> </header> <div class="grid-uniform"> <div class="grid-item small--one-half medium--one-quarter large--one-quarter"> <a href="/products/emirates-bio-farm-organic-beetroot" class="product-grid-item"> <div class="product-grid-image" style="height: 228px;"> <div class="product-grid-image--centered"> <div class="lazyload__image-wrapper no-js" style="max-width: 250px"> <div style="padding-top:100.0%;"> <img class="no-js lazyautosizes lazyloaded" src="//cdn.shopify.com/s/files/1/0543/3760/1723/products/Fff_Zing_O_Wings.jpg?v=1620624250" alt="Emirates Bio Farm Organic Beetroot" > </div></div></div></div><p>Emirates Bio Farm Organic Beetroot</p><h4>500g pack</h4> <div class="product-item--price"> <span class="h1 medium--left"> <span class="visually-hidden">Regular price</span> <small aria-hidden="true">Dhs. 13.00</small> <span class="visually-hidden">Dhs. 13.00</span> </span> </div></a> </div></div></div></div></div>';
+
+    splittedInpValue = searchString.split(",");
+    splittedInpValue = splittedInpValue.map(split => split.trim())
+    for (let ittr = 0; ittr < splittedInpValue.length; ittr++) {
+        let val = splittedInpValue[ittr];
+        if (!val) return false;
+
+        if (typeof totalSearchRes[val] != "undefined") {
+            let ind = splittedInpValue.indexOf(val);
+            let modifiedVal = [];
+            for (let k = 0; k < splittedInpValue.length; k++) {
+                if (k <= ind) {
+                    modifiedVal.push(splittedInpValue[k])
+                }
+            }
+            finalResult = getFinalResult(totalSearchRes, modifiedVal);
+            drawAutoComplete(finalResult, ittr);
+        } else {
+            $.ajax({
+                url: "https://groceryfinder.myshopify.com/search",
+                data: {
+                    q: val,
+                    type: "product",
+                    view: "json",
+                },
+                dataType: "json",
+                success: function (data) {
+                    processor(data, ittr, val, splittedInpValue);
+                }
+            });
+        }
+
+    }
+
+    function processor(data, ittr, val, splittedInpValue) {
+        let currentRes = []
+        let vendors = [];
+        //console.log(data);
+        vendors = data.map(el => el.vendor);
+        vendors = vendors.filter((v, i, a) => a.indexOf(v) === i);
+        if (typeof allVendors[ittr] == "undefined") {
+            allVendors.push(vendors);
+        } else {
+            allVendors[ittr] = vendors;
+        }
+        for (let i = 0; i < vendors.length; i++) {
+            let tem = {};
+            tem[vendors[i]] = [];
+            currentRes.push(tem);
+        }
+        for (let i = 0; i < data.length; i++) {
+            currentRes = buildCurrentRes(data[i].vendor, data[i], currentRes);
+        }
+        totalSearchRes[val] = currentRes;
+        // console.log('toatla res ',  totalSearchRes[val]);
+        finalResult = getFinalResult(totalSearchRes, splittedInpValue);
+        removeDuplicate(finalResult);
+        drawAutoComplete(finalResult, ittr);
+    }
+
+
+    function buildCurrentRes(vendor, productData, resArr) {
+        for (let i = 0; i < resArr.length; i++) {
+            if (Object.keys(resArr[i])[0] == vendor) {
+                resArr[i][`${vendor}`].push(productData)
+            }
+        }
+        return resArr;
+    }
+
+    function getFinalResult(allRes, splitedVal) {
+        let mergedContainer = {};
+        splitedVal.forEach(inputNo => {
+            // console.log('matched res ',allRes[inputNo])
+            for (let i = 0; i < allRes[inputNo].length; i++) {
+                let sObj = Object.entries(allRes[inputNo][i]);
+                if (typeof mergedContainer[`${sObj[0][0]}`] == "undefined") {
+                    mergedContainer[`${sObj[0][0]}`] = sObj[0][1]
+
+                } else {
+                    mergedContainer[`${sObj[0][0]}`] = mergedContainer[`${sObj[0][0]}`].concat(sObj[0][1]);
+                }
+            }
+        });
+        return mergedContainer;
+    }
+
+    function removeDuplicate(results) {
+        if (typeof results == "object") {
+            let resultSeperated = Object.entries(results);
+            resultSeperated.forEach(res => {
+                let key = res[0];
+                let values = res[1];
+                let ids = values.map(obj => obj.id);
+                let duplicates = [];
+                ids.sort(function (a, b) { return a - b });
+                for (let i = 0; i < ids.length - 1; i++) {
+                    if (ids[i] == ids[i + 1]) {
+                        duplicates.push(ids[i + 1])
+                    }
+                }
+                for (let i = values.length - 1; i >= 0; i--) {
+                    if (duplicates.includes(values[i].id)) {
+                        let temId = values[i].id;
+                        values.splice(i, 1);
+                        for (let j = 0; j < duplicates.length; j++) {
+                            if (duplicates[j] == temId) {
+                                duplicates.splice(j, 1);
+                            }
+                        }
+                    }
+                }
+
+                results[`${key}`] = values
+
+            })
+        }
+        return results;
+    }
+
 
     function matulGetParameterByName(name, url = window.location.href) {
         name = name.replace(/[\[\]]/g, '\\$&');
